ea848f09b74ffc81710129fbef8b0d929862a856
Alucard <Vinicius-Mazza>
2020-07-16 10:30:04 -0300

    Acessando e exibindo produtos

---
+++ frontend/src/app/components/products/products-read/products-read.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../product.model';
+import { ProductService } from '../product.service';
+
+@Component({
+  selector: 'app-products-read',
+  templateUrl: './products-read.component.html',
+  styleUrls: ['./products-read.component.css']
+})
+export class ProductsReadComponent implements OnInit {
+
+  products: Product[]
+
+  constructor(private productService: ProductService) { }
+
+  ngOnInit(): void {
+    this.productService.read().subscribe(products => {
+      this.products = products
+      console.log(products)
+    })
+  }
+
+}
